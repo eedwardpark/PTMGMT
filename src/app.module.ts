6338ff503e5953bb7743859821bb159634d22a54
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-02 21:09:35 -0700

    feat: connect to mySQL db through typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClinicModule } from './clinic/clinic.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',       
+      port: 3306,                 // your MySQL port
+      username: 'root',
+      password: process.env.MYSQL_PW, // using the correct environment variable
+      database: 'patient_management',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, 
+      // ⚠️ For production, switch synchronize:false and use migrations.
+    }),
+    ClinicModule,
+    PatientModule
+  ],
 })
 export class AppModule {}
