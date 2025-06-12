08ffb6fa678debb5a15ec718329e348cab636193
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-12 01:06:08 -0700

    refactor patient module to pass service and controller

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -1,9 +1,19 @@
+// src/patient/patient.module.ts
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Patient } from './entity/patient.entity';
+import { Clinic } from '../clinic/entity/clinic.entity';   // ← import Clinic here
 import { PatientService } from './patient.service';
 import { PatientController } from './patient.controller';
+import { ClinicModule } from '../clinic/clinic.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Patient, Clinic]),  
+    ClinicModule,
+  ],
   providers: [PatientService],
-  controllers: [PatientController]
+  controllers: [PatientController],
 })
 export class PatientModule {}
