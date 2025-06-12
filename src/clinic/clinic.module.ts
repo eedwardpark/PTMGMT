6254c3e2e67b2fc9af93e89c458d6c97d81df9f6
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-12 01:04:48 -0700

    feat:

--- src/clinic/clinic.module.ts
+++ src/clinic/clinic.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClinicService } from './clinic.service';
 import { ClinicController } from './clinic.controller';
+import { Clinic } from './entity/clinic.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Clinic])],
   providers: [ClinicService],
-  controllers: [ClinicController]
+  controllers: [ClinicController],
+  exports: [ClinicService],
 })
 export class ClinicModule {}
