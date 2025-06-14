bac302061050851349d39b78c5d50438695ccafe
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-13 23:13:04 -0700

    feat Patient model creatd in FE

---
+++ FE/src/models/Patient.ts
@@ -0,0 +1,15 @@
+import type { Clinic } from "./Clinic";
+
+export interface Patient {
+  id: number;
+  clinic: Clinic;
+  ltc: string | null;
+  lastName: string;
+  firstName: string;
+  middleName: string | null;
+  gender: 'M' | 'F';
+  dateOfBirth: string;
+  phone: string;
+  ehrId: string;
+  createdAt: string;
+}
