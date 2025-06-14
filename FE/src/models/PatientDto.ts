527af66228d5e03c4fea3a82026db99e69982226
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-13 23:15:27 -0700

    feat patientDto created in FE

---
+++ FE/src/models/PatientDto.ts
@@ -0,0 +1,11 @@
+export interface PatientDto {
+  clinicAcronym: string;
+  ltc?: string;
+  lastName: string;
+  firstName: string;
+  middleName?: string;
+  gender: 'M' | 'F';
+  dateOfBirth: string;
+  phone: string;
+  ehrId?: string;
+}
