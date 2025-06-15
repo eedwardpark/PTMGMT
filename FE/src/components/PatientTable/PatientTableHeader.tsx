881cc5f198d037c7a233d6d658113ccac32b8a0f
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-15 14:11:34 -0700

    feat PatientTableHeader component

---
+++ FE/src/components/PatientTable/PatientTableHeader.tsx
@@ -0,0 +1,58 @@
+import { TableHead, TableRow, TableCell, TableSortLabel, Checkbox } from '@mui/material';
+import type { TableColumn } from './interface/TableConfiguration';
+
+interface PatientTableHeaderProps<T> {
+  columns: TableColumn<T>[];
+  orderBy: keyof T;
+  orderDir: 'asc' | 'desc';
+  onSort: (column: keyof T) => void;
+  selectionState: {
+    isAllSelected: boolean;
+    isIndeterminate: boolean;
+  };
+  onSelectAll: (checked: boolean) => void;
+}
+
+export const PatientTableHeader = <T,>({
+  columns,
+  orderBy,
+  orderDir,
+  onSort,
+  selectionState,
+  onSelectAll,
+}: PatientTableHeaderProps<T>) => (
+  <TableHead>
+    <TableRow>
+      <TableCell padding="checkbox" sx={{ backgroundColor: 'grey.50' }}>
+        <Checkbox
+          indeterminate={selectionState.isIndeterminate}
+          checked={selectionState.isAllSelected}
+          onChange={(e) => onSelectAll(e.target.checked)}
+          size="small"
+        />
+      </TableCell>
+      {columns.map((column) => (
+        <TableCell
+          key={String(column.id)}
+          sx={{
+            minWidth: column.minWidth,
+            backgroundColor: 'grey.50',
+            fontWeight: 600,
+          }}
+        >
+          {column.sortable ? (
+            <TableSortLabel
+              active={orderBy === column.id}
+              direction={orderDir}
+              onClick={() => onSort(column.id as keyof T)}
+            >
+              {column.label}
+            </TableSortLabel>
+          ) : (
+            column.label
+          )}
+        </TableCell>
+      ))}
+    </TableRow>
+  </TableHead>
+);
