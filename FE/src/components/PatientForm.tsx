0265b8237fdff4c823b4e2a49d8cf329ab9c9a7b
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-15 21:08:28 -0700

    delete refactored File

--- FE/src/components/PatientForm.tsx
+++
@@ -1,9 +0,0 @@
-export const PatientForm: React.FC = () => {
-  return (
-    <div>
-      <h1>Patient Form</h1>
-      <p>This is where the patient form will be implemented.</p>
-      {/* Form implementation goes here */}
-    </div>
-  );
-};
