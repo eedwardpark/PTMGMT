f9a7e8c4c9bfd8f77f95de35a48ec7c12beb1e95
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-17 15:59:57 -0700

    refactor miaints for app listen

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -5,10 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173', // for local dev
-      'http://hicaretoyprojectbucket.s3-website-us-east-1.amazonaws.com', // S3 bucket
-    ],
+    origin: 'http://hicaretoyprojectbucket.s3-website-us-east-1.amazonaws.com',
   });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
