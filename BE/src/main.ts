25c332f5dd4166904d2740f6165ba55fdc820898
eedwardpark <32019573+eedwardpark@users.noreply.github.com>
2025-06-17 15:56:53 -0700

    refactor maints in BE for s3 bucket

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:5173' });
-
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // for local dev
+      'http://hicaretoyprojectbucket.s3-website-us-east-1.amazonaws.com', // S3 bucket
+    ],
+  });
 }
 bootstrap();
-
